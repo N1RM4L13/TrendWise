8b0266e1f2f2fe720b7b863754063d0c2cc181d5
Nirmal <n1rm4l13@gmail.com>
2025-01-19 17:35:38 +0530

    data unzip

--- data/download_data.py
+++ data/download_data.py
@@ -13,10 +13,15 @@ os.environ['KAGGLE_USERNAME'] = settings.KAGGLE_USERNAME
 os.environ['KAGGLE_KEY'] = settings.KAGGLE_KEY.get_secret_value()
 
 # Define the target directory for downloading the dataset
-download_dir = Path(__file__).resolve().parent / "data"
+download_dir = Path(__file__).resolve().parent
 
 # Run the Kaggle command to download the dataset into the data/ folder
 os.system(f"kaggle competitions download -c h-and-m-personalized-fashion-recommendations -p {download_dir}")
+
+for zip_file in download_dir.glob("*.zip"):
+    print(f"Unzipping {zip_file}...")
+    os.system(f'tar -xf "{zip_file}" -C "{download_dir}"')  
+
 
 
 
